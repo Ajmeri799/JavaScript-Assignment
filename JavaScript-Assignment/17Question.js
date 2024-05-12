fc0ac15fba710593a43424c67d59efbee286f875
Ajmeri Gudiya <149288719+Ajmeri799@users.noreply.github.com>
2024-05-12 21:25:00 +0530

    17Question.js

--- JavaScript-Assignment/17Question.js
+++ JavaScript-Assignment/17Question.js
@@ -4,5 +4,12 @@
 //     - DD/MM/YYYY HH:mm
 
 let date = new Date();
-console.log(date.toLocaleDateString());
-console.log(date.toDateString());
+console.log(
+  `${date.toLocaleDateString()}  ${date.getHours()}:${date.getMinutes()}`
+);
+console.log(
+  `${date.getFullYear()}-${date.getMonth()}-${date.getDate()}  ${date.getHours()}:${date.getMinutes()}`
+);
+console.log(
+  `${date.getDate()}-${date.getMonth()}-${date.getFullYear()}  ${date.getHours()}:${date.getMinutes()}`
+);
